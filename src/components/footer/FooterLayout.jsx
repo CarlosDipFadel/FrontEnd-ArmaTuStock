ab692e38f79a4bab129328e979f08ea7d4d3e9f5
Mario Copa <mario.mec887@gmail.com>
2023-08-09 22:19:47 -0300

    nuevos estilos a footer

--- src/components/footer/FooterLayout.jsx
+++ src/components/footer/FooterLayout.jsx
@@ -1,112 +1,113 @@
-import React from 'react';
+import React from "react";
 
-import { 
+import {
   MDBFooter,
   MDBContainer,
   MDBIcon,
   MDBInput,
   MDBCol,
   MDBRow,
-  MDBBtn 
-} from 'mdb-react-ui-kit';
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
-const FooterLayout  = () => {
+const FooterLayout = () => {
   return (
-    <MDBFooter className='text-center text-lg-start text-white'>
-      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
-        <div className='me-5 d-none d-lg-block'>
-          <span>Get connected with us on social networks:</span>
+    <MDBFooter className="text-center text-lg-start text-white">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Conéctate con nosotras en las redes sociales:</span>
         </div>
 
         <div>
-          <a href='' className='me-4 text-reset'>
+          <a href="" className="me-4 text-reset">
             <MDBIcon fab icon="facebook-f" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href="" className="me-4 text-reset">
             <MDBIcon fab icon="twitter" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href="" className="me-4 text-reset">
             <MDBIcon fab icon="google" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href="" className="me-4 text-reset">
             <MDBIcon fab icon="instagram" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href="" className="me-4 text-reset">
             <MDBIcon fab icon="linkedin" />
           </a>
-          <a href='' className='me-4 text-reset'>
+          <a href="" className="me-4 text-reset">
             <MDBIcon fab icon="github" />
           </a>
         </div>
       </section>
 
-      <section className=''>
-        <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>
+      <section className="">
+        <MDBContainer className="text-center text-md-start mt-5">
+          <MDBRow className="mt-3">
+            <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
                 <MDBIcon icon="gem" className="me-3" />
                 #ArmaTuStock
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-                consectetur adipisicing elit.
+                Somos un grupo de apasionados por el desarrollo de software y la
+                creación de soluciones tecnológicas innovadoras para resolver
+                los desafíos del mundo actual.
               </p>
             </MDBCol>
 
-            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+            <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Productos</h6>
               <p>
-                <a href='#!' className='text-reset'>
-                  Angular
+                <a href="#!" className="text-reset">
+                  Quienes Somos
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  React
+                <a href="#!" className="text-reset">
+                  Prensa
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Vue
+                <a href="#!" className="text-reset">
+                  Proveedores
                 </a>
               </p>
               <p>
-                <a href='#!' className='text-reset'>
-                  Laravel
-                </a>
-              </p>
-            </MDBCol>
-
-            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Pricing
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Settings
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href='#!' className='text-reset'>
-                  Help
+                <a href="#!" className="text-reset">
+                  Inversores
                 </a>
               </p>
             </MDBCol>
 
-            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+            <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Enlaces útiles</h6>
+              <p>
+                <a href="#!" className="text-reset">
+                  Precios
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Ajustes
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Pedidos
+                </a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">
+                  Ayuda
+                </a>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">Contacto</h6>
               <p>
                 <MDBIcon icon="home" className="me-2" />
-                Gral. Paz 576, T4000 SM de Tucumán
+                Gral. Paz 576, T4000 SM de Tucumán, Tucuman
               </p>
               <p>
                 <MDBIcon icon="envelope" className="me-3" />
@@ -123,16 +124,17 @@ const FooterLayout  = () => {
         </MDBContainer>
       </section>
 
-      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
+      >
         © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          Bootstrap.com
+        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
+          tustock.com
         </a>
       </div>
     </MDBFooter>
   );
-}
-
-
+};
 
 export default FooterLayout;
