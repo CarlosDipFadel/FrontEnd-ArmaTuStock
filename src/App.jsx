08c4df1840b39325570e5af259efd52fb683185c
Mario Copa <mario.mec887@gmail.com>
2023-08-17 22:42:04 -0300

    Creacion pagina Acerca de Nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import UserContext from "./context/UserContext";
 import Routes from "./routes/Routes";
 import Header from "./components/header/Header";
 import FooterLayout from "./components/footer/FooterLayout";
+import Nosotros from "./components/nosotros/Nosotros";
+
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,10 +17,12 @@ function App() {
       <UserContext>
         <div className="header"><Header /></div>
         <div className="body"><Routes /></div>
+        <div className="nosotros"><Nosotros /></div>
         <div className="footer"><FooterLayout /></div>
       </UserContext>
     </>
   );
 }
+
 
 export default App;
