9869011a50fcc91555ee098883fff3048806246e
CarlosFadel <carlitosfadel.cdf@gmail.com>
2023-08-29 20:43:46 -0300

    Modifico input para home

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserContext from "./context/UserContext";
 import Routes from "./routes/Routes";
-import Navbar from "./components/Header/Navbar";
 import FooterLayout from "./components/Footer/FooterLayouts";
-import Header from "./components/Header/Navbar"
+import Header from "./components/Header/Header"
 
 
-=======
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Routes from "./routes/Routes";
-import FooterLayout from "./components/footer/FooterLayouts";
-import Header from "./components/header/Header";
-import UserContext  from "./context/UserContext";
->>>>>>> ca9256383f4fb3921a1aef8564addb22bc600d1f
 
 function App() {
   return (
     <>
       <UserContext>
-<<<<<<< HEAD
-        <div className="Navbar"><Navbar /></div>
-        <div className="body"><Routes /></div>
-=======
-        <div className="header"> <Header /></div>
+        <div className="Header"> <Header /></div>
         <div className="body"> <Routes/></div>
->>>>>>> ca9256383f4fb3921a1aef8564addb22bc600d1f
         <div className="footer"><FooterLayout /></div>
       </UserContext>
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,63 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import './Home.css';
 
-<<<<<<< HEAD
-const WelcomePage = () => {
-  return (
-    <div className='Encabezado' >
-      <h1>Bienvenido a la Ferretería</h1>
-      <div>
-        <Button href="/show" variant="link">
-          <img src="/ruta/a/imagen/listado-de-productos.jpg" alt="Listado de productos" />
-        </Button>
-        <Button href="/Create" variant="link">
-          <img src="/ruta/a/imagen/crear-producto.jpg" alt="Crear producto" />
-        </Button>
-      </div>
-    </div>
-  );
-};
-=======
-const Home = () => {
-<<<<<<< HEAD
-    
-=======
->>>>>>> ca9256383f4fb3921a1aef8564addb22bc600d1f
+
+
+const Bienvenido = () => {
     return (
-        <div>
-            <h1 style={{ textAlign: 'center' }}> Bienvenido </h1>
+        <div className='Encabezado' >
+            <h1>Bienvenido a la Ferretería</h1>
+            <div>
+                <Button href="/show" variant="link">
+                    <img src="/ruta/a/imagen/listado-de-productos.jpg" alt="Listado de productos" />
+                </Button>
+
+
+
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button href="/show" variant="link">
+                            < img alt="Listado de productos" fluid />
+                        </Button>
+                    </Card.Body>
+                </Card>
+
+
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </Card.Text>
+                        <Button href="/Create" variant="link">
+                            <img alt="Crear producto" fluid />
+                        </Button>
+                    </Card.Body>
+                </Card>
+
+            </div>
         </div>
     );
-}
->>>>>>> 7f83dd3c79eb1662b05f8b5f2d139a47ece9feba
+};
 
-export default WelcomePage;
+return (
+    <div>
+        <h1 style={{ textAlign: 'center' }}> Bienvenido </h1>
+    </div>
+);
+
+
+
+export default Bienvenido;
