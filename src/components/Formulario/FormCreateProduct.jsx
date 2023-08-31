4b0916e68124302a20aa572507093f8154c141af
CarlosFadel <carlitosfadel.cdf@gmail.com>
2023-08-31 18:40:34 -0300

    Modifico css de pagina home, faltaria agregarle una imagen, y la funcionalidad esta ok

--- src/components/Formulario/FormCreateProduct.jsx
+++ src/components/Formulario/FormCreateProduct.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup' ;
+import * as Yup from 'yup';
 import Button from 'react-bootstrap/Button';
 import FormBs from 'react-bootstrap/Form';
 import './formulario.css'
@@ -12,99 +12,93 @@ const FormCreateProduct = () => {
         name: '',
         description: '',
         image: '',
-        stock: 0,
-        price: 0
+        stock: '',
+        price: ''
     }
 
     const formSchema = Yup.object().shape({
-        name: Yup.string().min(4, 'nombre demasiado corto').max(20, 'nombre demasiado largo').required('el campo es obligatorio'),
-        description:  Yup.string().min(6, 'descripcion demasiado corta').max(100, 'descripcion demasiado larga').required('el campo es obligatorio'),
+        name: Yup.string().min(4, 'nombre demasiado corto').max(20, 'nombre demasiado largo').required('el campo el obligatorio'),
+        description: Yup.string().min(10, 'descripcion demasiado corta').max(100, 'desciprcion demasiado largo').required('el campo el obligatorio'),
         image: Yup.string().required('el campo es obligatorio'),
         stock: Yup.number().required('el campo es obligatorio'),
-        price: Yup.number().required('el campo es obligatorio') 
+        price: Yup.number().required('el campo es obligatorio')
     })
     const submit = (values) => {
         console.log(values);
-        axios.post(`${import.meta.env.VITE_URL}/api/products/register/product`,{
+        axios.post(`${import.meta.env.VITE_URL}/api/products/register/product`, {
             nombre: values.name,
             descripcion: values.description,
             stock: values.stock,
             precio: values.price
-        }).catch((error)=>{
+        }).catch((error) => {
             console.log(error);
         })
     }
     return (
         <div className='container'>
-            <Formik 
+            <Formik
                 initialValues={initialCredentials}
                 validationSchema={formSchema}
                 onSubmit={submit}
             >
-                {
-                    ({values, errors, touched, isSubmitting}) => (
-                        <Form>
-                            <FormBs.Group className='mb-3'>
-                                <label htmlFor='name'> Nombre del producto </label>
-<<<<<<< HEAD
-                                <Field id='name' type='text' placeholder='Destornillador' name='name' className='form-control field-input'/>
-=======
-                                <Field id='nombre' type='text' placeholder='Buzo' name='name' className='form-control field-input'/>
->>>>>>> ca9256383f4fb3921a1aef8564addb22bc600d1f
-                                {
-                                    errors.name && touched.name && (
-                                    <ErrorMessage name='name' component='div'></ErrorMessage>  
-                                    )
-                                }
-                            </FormBs.Group>
+                {({ values, errors, touched, isSubmitting }) => (
+                    <Form>
+                        <FormBs.Group className='mb-3'>
+                            <label htmlFor='name'> Nombre del producto </label>
 
-                            <FormBs.Group className='mb-3'>
-                                <label htmlFor='description'> descripcion  </label>
-<<<<<<< HEAD
-                                <Field id='description' type='text' placeholder='Destornillador torx 8' name='description' className='form-control field-input'/>
-=======
-                                <Field id='descripcion' type='text' placeholder='Buzo comodo ideal para invierno' name='description' className='form-control field-input'/>
->>>>>>> ca9256383f4fb3921a1aef8564addb22bc600d1f
-                                {
-                                    errors.description && touched.description && (
-                                    <ErrorMessage name='description' component='div'></ErrorMessage>  
-                                    )
-                                }
-                            </FormBs.Group>
-                            <FormBs.Group className='mb-3'> 
-                                <label htmlFor='image'> imagen </label>
-                                <Field id='image' type='text' placeholder='imagen' name='image' className='form-control field-input'/>
-                                {
-                                    errors.image && touched.image && (
-                                    <ErrorMessage name='image' component='div'></ErrorMessage>  
-                                    )
-                                }
-                            </FormBs.Group>
-                            <FormBs.Group className='mb-3'> 
-                                <label htmlFor='stock'> stock </label>
-                                <Field id='stock' type='number' placeholder='5' name='stock' className='form-control field-input'/>
-                                {
-                                    errors.stock && touched.stock && (
-                                    <ErrorMessage name='stock' component='div'></ErrorMessage>  
-                                    )
-                                }
-                            </FormBs.Group>
-                            <FormBs.Group className='mb-3'> 
-                                <label htmlFor='price'> precio </label>
-                                <Field id='precio' type='number' placeholder='8000' name='price' className='form-control field-input'/>
-                                {
-                                    errors.price && touched.price && (
-                                    <ErrorMessage name='price' component='div'></ErrorMessage>  
-                                    )
-                                }
-                            </FormBs.Group>
+                            <Field id='name' type='text' placeholder='Destornillador' name='name' className='form-control field-input' />
+
+
+                            {
+                                errors.name && touched.name && (
+                                    <ErrorMessage name='name' component='div'></ErrorMessage>
+                                )
+                            }
+                        </FormBs.Group>
+
+                        <FormBs.Group className='mb-3'>
+                            <label htmlFor='description'> descripcion  </label>
+                            <Field id='descripcion' type='text' placeholder='Buzo comodo ideal para invierno' name='description' className='form-control field-input' />
+                            {
+                                errors.description && touched.description && (
+                                    <ErrorMessage name='description' component='div'></ErrorMessage>
+                                )
+                            }
+                        </FormBs.Group>
+                        <FormBs.Group className='mb-3'>
+                            <label htmlFor='image'> imagen </label>
+                            <Field id='image' type='text' placeholder='imagen' name='image' className='form-control field-input' />
+                            {
+                                errors.image && touched.image && (
+                                    <ErrorMessage name='image' component='div'></ErrorMessage>
+                                )
+                            }
+                        </FormBs.Group>
+                        <FormBs.Group className='mb-3'>
+                            <label htmlFor='stock'> stock </label>
+                            <Field id='stock' type='number' placeholder='5' name='stock' className='form-control field-input' />
+                            {
+                                errors.stock && touched.stock && (
+                                    <ErrorMessage name='stock' component='div'></ErrorMessage>
+                                )
+                            }
+                        </FormBs.Group>
+                        <FormBs.Group className='mb-3'>
+                            <label htmlFor='price'> precio </label>
+                            <Field id='precio' type='number' placeholder='8000' name='price' className='form-control field-input' />
+                            {
+                                errors.price && touched.price && (
+                                    <ErrorMessage name='price' component='div'></ErrorMessage>
+                                )
+                            }
+                        </FormBs.Group>
 
                         <Button className='btn btn-primary' type="submit">Cargar producto</Button>
                         {
                             isSubmitting ? (<p>  Enviando producto </p>) : null
                         }
-                        </Form>
-                    )   
+                    </Form>
+                )
                 }
 
             </Formik>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './Home.css';
@@ -11,52 +11,37 @@ const Bienvenido = () => {
         <div className='Encabezado' >
             <h1>Bienvenido a la Ferretería</h1>
             <div>
-                <Button href="/show" variant="link">
-                    <img src="/ruta/a/imagen/listado-de-productos.jpg" alt="Listado de productos" />
-                </Button>
-
-
-
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button href="/show" variant="link">
-                            < img alt="Listado de productos" fluid />
-                        </Button>
-                    </Card.Body>
-                </Card>
-
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                        </Card.Text>
-                        <Button href="/Create" variant="link">
-                            <img alt="Crear producto" fluid />
-                        </Button>
-                    </Card.Body>
-                </Card>
-
+                <container fluid>
+                    <row>
+                        <col1>
+                            <Card className='Card1' style={{ width: '18rem' }} fluid>
+                                <Card.Img variant="top" src="holder.js/100px180"  />
+                                <Card.Body>
+                                    <Card.Title>Aqui estan los productos</Card.Title>
+                                    <Button href="/show" variant="link">
+                                        < img alt="Listado de productos" fluid />
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </col1>
+                        <col2>
+                            <Card className='Card2' style={{ width: '18rem'}} fluid>
+                                <Card.Img variant="top" src="holder.js/100px180"  />
+                                <Card.Body>
+                                    <Card.Title>Crear producto</Card.Title>
+                                   <Button href="/Create" variant="link">
+                                        <img alt="Crear producto" fluid />
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </col2>
+                    </row>
+                </container>
             </div>
         </div>
     );
 };
 
-return (
-    <div>
-        <h1 style={{ textAlign: 'center' }}> Bienvenido </h1>
-    </div>
-);
 
 
 
