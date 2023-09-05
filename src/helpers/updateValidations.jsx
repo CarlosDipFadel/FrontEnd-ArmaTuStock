6fc63583f2f3f2e751cf92d7066ca9012b4b69ae
AgustinSanchez1 <agustinsanchezdev@gmail.com>
2023-09-05 19:08:25 -0300

    Modificaciones update usuarios

---
+++ src/helpers/updateValidations.jsx
@@ -0,0 +1,32 @@
+import * as Yup from "yup";
+
+export const updateValidationSchema = Yup.object().shape({
+    nombre: Yup.string()
+        .required("Campo requerido")
+        .max(30, "Debe tener 30 caracteres o menos"),
+    apellido: Yup.string()
+        .required("Campo requerido")
+        .max(30, "Debe tener 30 caracteres o menos"),
+    email: Yup.string()
+        .email("Correo electrónico inválido")
+        .required("Campo requerido"),
+    telefono: Yup.string()
+        .required("El teléfono es obligatorio")
+        .matches(/^\d+$/, "Ingrese un número de teléfono válido"),
+    direccion: Yup.string()
+        .required("La dirección es obligatoria")
+        .max(255, "La dirección no debe exceder los 255 caracteres"),
+    codigoPostal: Yup.string()
+        .required("El código postal es obligatorio")
+        .matches(/^\d{4}$/, "Ingrese un código postal válido de 4 dígitos"),
+    userName: Yup.string()
+        .required("El nombre de usuario es obligatorio")
+        .min(4, "El nombre de usuario debe tener al menos 4 caracteres")
+        .max(20, "El nombre de usuario no debe exceder los 20 caracteres"),
+    password: Yup.string()
+        .min(8, "La contraseña debe tener al menos 8 caracteres")
+        .required("Campo requerido"),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "Las contraseñas no coinciden")
+        .required("Campo requerido")
+});
--- src/pages/registerEdit/RegisterEdit.jsx
+++ src/pages/registerEdit/RegisterEdit.jsx
@@ -4,44 +4,49 @@ import { Form as FormBoostrap } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
-import { validationSchema } from "../../helpers/registerValidations";
+import { updateValidationSchema } from "../../helpers/updateValidations";
 import logo from "../../img/Logo.png";
 import * as Yup from "yup";
 import clsx from "clsx";
-import {UsuariosContext} from "../../context/UserContext"
+import { UsuariosContext } from "../../context/UserContext";
 
 const RegisterEdit = () => {
   const location = useLocation();
   const [roles, setRoles] = useState([]);
-  const [roleSelected, setRoleSelected] = useState()
-  const [user, setUser] = useState(location.state || {});
+  const [roleSelected, setRoleSelected] = useState();
+  const [initialValues, setInitialValues] = useState();
 
-  const {getRoles, updateUser} = useContext(UsuariosContext)
+  const { getRoles, updateUser } = useContext(UsuariosContext);
 
   useEffect(() => {
-    if (user.roles && user.roles.length > 0) {
-      setRoleSelected(user.roles[0].role);
-    }
-    getRoles().then((response)=>{
-      setRoles(response)
-    }).catch((error)=>{
-      console.log(error);
-    })
+    setInitialValues({
+      nombre: location.state.nombre || "",
+      apellido: location.state.apellido || "",
+      email: location.state.email || "",
+      telefono: location.state.telefono || "",
+      direccion: location.state.direccion || "",
+      codigoPostal: location.state.codigoPostal || "",
+      userName: location.state.userName || "",
+      password: "",
+      confirmPassword: "",
+      rol: location.state.roles && location.state.roles[0] ? location.state.roles[0].role : "",
+    });
   }, []);
 
-  const initialValues = {
-    nombre: user.nombre || '',
-    apellido: user.apellido || '',
-    email: user.email || '',
-    confirmEmail: user.email || '',
-    telefono: user.telefono || '',
-    direccion: user.direccion || '',
-    codigoPostal: user.codigoPostal || '',
-    userName: user.userName || '',
-    password: "",
-    confirmPassword: "",
-    rol: user.roles && user.roles[0] ? user.roles[0].role : "",
-  };
+  useEffect(() => {
+    if (initialValues) {
+      setRoleSelected(initialValues.rol || "");
+
+      getRoles()
+        .then((response) => {
+          setRoles(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [initialValues]);
+
   const handleFormSubmit = async (values) => {
     try {
       Swal.fire({
@@ -53,9 +58,9 @@ const RegisterEdit = () => {
         confirmButtonText: "OK!",
       }).then(async (result) => {
         if (result.isConfirmed) {
-          try { 
+          try {
             const userDB = {
-              _id: user._id,
+              _id: location.state._id,
               nombre: values.nombre,
               apellido: values.apellido,
               email: values.email,
@@ -67,7 +72,7 @@ const RegisterEdit = () => {
               roles: [roleSelected],
             };
 
-            updateUser(userDB).then((response)=>{
+            updateUser(userDB).then((response) => {
               if (response.status === 201) {
                 Swal.fire(
                   response.data,
@@ -75,15 +80,13 @@ const RegisterEdit = () => {
                   "success"
                 );
               } else {
-                console.log("assdasdasdasdasd");
                 Swal.fire(
                   "Error",
                   "Hubo un error al registrar el usuario en la base de datos.",
                   "error"
                 );
               }
-            })
-
+            });
           } catch (error) {
             console.error("Error al registrar el usuario:", error);
             // SweetAlert2 para mostrar un mensaje de error
@@ -99,184 +102,222 @@ const RegisterEdit = () => {
       console.log(error);
     }
   };
+
   return (
+    <>
+      {initialValues ? (
+        <Formik
+          initialValues={initialValues}
+          validationSchema={updateValidationSchema}
+          onSubmit={handleFormSubmit}
+        >
+          {(props) => (
+            <Form className="container mt-5 p-4 rounded shadow text-white" style={{ backgroundColor: "#01939c" }}>
 
+              <div className="welcome-message text-center mb-4">
+                <h3>Formulario de registro</h3>
+              </div>
 
-    <Formik initialValues={initialValues} validationSchema={validationSchema}  onSubmit={handleFormSubmit}>
-      <Form
-        className="container mt-5 p-4 rounded shadow text-white"
-        style={{ backgroundColor: '#01939c' }}
-      >
-        <div className="welcome-message text-center mb-4">
-          <h3>Formulario de registro</h3>
-        </div>
-        <div className="d-flex d-none d-sm-block text-center mb-4">
-          <img
-            src={logo}
-            alt="Logo"
-            className="img-fluid rounded-circle mt-4"
-            style={{ maxWidth: "300px", width: "100%", margin: "0 auto" }}
-          />
-        </div>
+              {/* Logo */}
 
-        <div className="form-group row">
-          <div className="col-md-6  mb-md-3 mb-sm-0 ">
-            <label htmlFor="nombre">Nombre:</label>
-            <Field
-              type="text"
-              id="nombre"
-              name="nombre"
-              className="form-control"
-            />
-            <ErrorMessage
-              name="nombre"
-              component="div"
-              className="text-danger"
-            />
-          </div>
-          <div className="col-md-6 mt-sm-0">
-            <label htmlFor="apellido">Apellido:</label>
-            <Field
-              type="text"
-              id="apellido"
-              name="apellido"
-              className="form-control"
-            />
-            <ErrorMessage
-              name="apellido"
-              component="div"
-              className="text-danger"
-            />
-          </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Correo electrónico:</label>
-          <Field
-            type="email"
-            id="email"
-            name="email"
-            className="form-control"
-          />
-          <ErrorMessage name="email" component="div" className="text-danger" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirmEmail">Confirmar correo electrónico:</label>
-          <Field
-            type="email"
-            id="confirmEmail"
-            name="confirmEmail"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="confirmEmail"
-            component="div"
-            className="text-danger"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="telefono">Teléfono:</label>
-          <Field
-            id="telefono"
-            name="telefono"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="telefono"
-            component="div"
-            className="text-danger"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="direccion">Dirección:</label>
-          <Field
-            type="text"
-            id="direccion"
-            name="direccion"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="direccion"
-            component="div"
-            className="text-danger"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="codigoPostal">Código Postal:</label>
-          <Field
-            type="text"
-            id="codigoPostal"
-            name="codigoPostal"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="codigoPostal"
-            component="div"
-            className="text-danger"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="userName">Nombre de Usuario:</label>
-          <Field
-            type="text"
-            id="userName"
-            name="userName"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="userName"
-            component="div"
-            className="text-danger"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Contraseña:</label>
-          <Field
-            type="password"
-            id="password"
-            name="password"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="password"
-            component="div"
-            className="text-danger"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirmPassword">Confirmar contraseña:</label>
-          <Field
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            className="form-control"
-          />
-          <ErrorMessage
-            name="confirmPassword"
-            component="div"
-            className="text-danger"
-          />
-        </div>
+              <div className="d-flex d-none d-sm-block text-center mb-4">
+                <img
+                  src={logo}
+                  alt="Logo"
+                  className="img-fluid rounded-circle mt-4"
+                  style={{ maxWidth: "300px", width: "100%", margin: "0 auto" }}
+                />
+              </div>
 
-        <div className="form-group">
-          <label htmlFor="rol">Rol:</label>
+              {/* nombre y apellido */}
 
-          <FormBoostrap.Select aria-label="Default select example" onChange={(value) => { setRoleSelected(value.target.value) }}>
-            <option>{roleSelected}</option>
-            {roles.map((rol, index) => {
-              return (<option key={index} value={rol.role}>{rol.role}</option>)
-            })}
-          </FormBoostrap.Select>
-          <ErrorMessage name="rol" component="div" className="text-danger" />
-        </div>
+              <div className="form-group row">
+                <div className="col-md-6  mb-md-3 mb-sm-0 ">
+                  <label htmlFor="nombre">Nombre:</label>
+                  <Field
+                    type="text"
+                    value={props.values.nombre}
+                    name="nombre"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="nombre"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+                <div className="col-md-6 mt-sm-0">
+                  <label htmlFor="apellido">Apellido:</label>
+                  <Field
+                    type="text"
+                    value={props.values.apellido}
+                    name="apellido"
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="apellido"
+                    component="div"
+                    className="text-danger"
+                  />
+                </div>
+              </div>
 
-        <div className="form-group text-center">
-          <button type="submit" className="btn btn-dark mt-2 btn-lg"style={{ width: "70%" }}>
-            Actualizar
-          </button>
-        </div>
-      </Form>
-    </Formik>
+              {/* Correo electronico */}
 
+              <div className="form-group">
+                <label htmlFor="email">Correo electrónico:</label>
+                <Field
+                  type="email"
+                  value={props.values.email}
+                  name="email"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Telefono */}
+
+              <div className="form-group">
+                <label htmlFor="telefono">Teléfono:</label>
+                <Field 
+                  id="telefono" 
+                  value={props.values.telefono}
+                  name="telefono" 
+                  className="form-control" 
+                />
+                <ErrorMessage
+                  name="telefono"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Direccion */}
+
+              <div className="form-group">
+                <label htmlFor="direccion">Dirección:</label>
+                <Field
+                  type="text"
+                  value={props.values.direccion}
+                  name="direccion"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="direccion"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Codigo Postal */}
+
+              <div className="form-group">
+                <label htmlFor="codigoPostal">Código Postal:</label>
+                <Field
+                  type="text"
+                  value={props.values.codigoPostal}
+                  name="codigoPostal"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="codigoPostal"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Nombre de usuario */}
+
+              <div className="form-group">
+                <label htmlFor="userName">Nombre de Usuario:</label>
+                <Field
+                  type="text"
+                  value={props.values.userName}
+                  name="userName"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="userName"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Contraseña */}
+
+              <div className="form-group">
+                <label htmlFor="password">Contraseña:</label>
+                <Field
+                  type="password"
+                  name="password"
+                  value={props.values.password}
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Confirmar contraseña */}
+
+              <div className="form-group">
+                <label htmlFor="confirmPassword">Confirmar contraseña:</label>
+                <Field
+                  type="password"
+                  value={props.values.confirmPassword}
+                  name="confirmPassword"
+                  className="form-control"
+                />
+                <ErrorMessage
+                  name="confirmPassword"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Rol */}
+
+              <div className="form-group">
+                <label htmlFor="rol">Rol:</label>
+                <Field as="select" name="rol" value={props.values.rol} className="form-control">
+                  {roles.map((rol, index)=>{
+                    return (
+                      <option key={index} value={rol.role}>
+                        {rol.role}
+                      </option>
+                    );
+                  })}
+                </Field>
+
+                <ErrorMessage
+                  name="rol"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+
+              {/* Button Submit */}
+
+              <div className="form-group text-center">
+                <button
+                  type="submit"
+                  className="btn btn-dark mt-2 btn-lg"
+                  style={{ width: "70%" }}
+                >
+                  Actualizar
+                </button>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      ) : null}
+    </>
   );
 };
 
